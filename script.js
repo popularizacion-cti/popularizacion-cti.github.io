1a638cd7bc2050056556fd77ee9674a7108e158c
popularizacion-cti <popularizacion@concytec.gob.pe>
2026-04-01 16:42:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 let slides = document.querySelectorAll(".slide");
-
 let index = 0;
 
 function showSlides(){
-
-slides.forEach(slide => slide.classList.remove("active"));
-
-index++;
-
-if(index > slides.length){
-index = 1;
+    slides.forEach(slide => slide.classList.remove("active"));
+    index++;
+    if(index > slides.length) { index = 1; }
+    slides[index-1].classList.add("active");
 }
 
-slides[index-1].classList.add("active");
-
-}
-
-setInterval(showSlides,5000);
+// Cambia de imagen cada 5 segundos
+setInterval(showSlides, 5000);
